3a4dd01c3d5b3cc15738af5c85ab0c81723b8ecd
zkybs <zkybs@foxmail.com>
2017-06-03 00:28:28 +0800

    nineGrid calcute

--- nineGrid.cpp
+++ nineGrid.cpp
@@ -22,38 +22,45 @@ public:
     }
     bool find(vector<int> q,vector<int> grid){
         int line=grid.size()/3;
-        if((!(grid.size()%3)) && line && !linecalcute(line,grid)) return false;
+        if((!(grid.size()%3)) && line && (!linecalcute(line-1,grid))) return false;
         if(grid.size()==9 && columcalcute(grid)) {
             for(int i=0;i<9;i++){
+                // cout<<grid[i]<<" "<<grid[i+1]<< " "<<grid[i+2]<<endl;
+                cout<<i<<endl;
                 cout<<grid[i]<<" "<<grid[++i]<< " "<<grid[++i]<<endl;
+                cout<<i<<endl;
             }
             return true;
         }
-        for(int i=0;i<q.size();i++){
+        if(grid.size()==2){
             vector<int> ngrid(grid);
-            int num=q[i];
-            vector<int> nq(q); 
-            if(ngrid.size()==2)
-                ngrid.push_back(9);
-            else{
+            ngrid.push_back(9);
+            if(find(q,ngrid))
+                return true;
+        }else {
+            for(int i=0;i<q.size();i++){
+                vector<int> ngrid(grid);
+                int num=q[i];
+                vector<int> nq(q); 
                 ngrid.push_back(num);
                 nq.erase(nq.begin()+i);
-            }  
-            if(find(nq,ngrid))
-                return true;
+                if(find(nq,ngrid))
+                    return true;
+            }
         }
         return false;
     }
     bool linecalcute(int l,vector<int> &grid){
+        
         switch(l){
             case 0: return (grid[0]+grid[1]-9)==4;
-            case 1: return (grid[3]+grid[4])*grid[5]==4;
+            case 1: return ((grid[3]-grid[4])*grid[5])==4;
             case 2: return (grid[6]+grid[7]-grid[8])==4;
         }
     }
     bool columcalcute(vector<int> &grid){
-        return (grid[0]+grid[3])/grid[6]==4 &&\
-                (grid[1]-grid[4])*grid[7]==4 &&\
+        return ((grid[0]+grid[3])/grid[6])==4 &&\
+                ((grid[1]-grid[4])*grid[7])==4 &&\
                 (grid[2]-grid[5]-grid[8])==4;
     }
 };
